6bd903095ce4be366e26cc1a7a18891f49f7691d
futurepaul <futurepaul@ip-192-168-2-110.ec2.internal>
2015-04-24 10:53:01 -0700

    dispatch from a list

--- lib/notes.ex
+++ lib/notes.ex
@@ -7,21 +7,19 @@ defmodule Notes do
   end
   defp parse_args([target|_]) do
     target
+    |> String.split(".")
+    |> List.first
   end
   defp write_loop(target) do
     case target do
-      "notes" -> {:ok, file} = File.open("notes.txt", [:append])
-      "philosophy" ->  {:ok, file} = File.open("philosophy.txt", [:append])
-      "science" -> {:ok, file} = File.open("science.txt", [:append])
-      "programming" -> {:ok, file} = File.open("programming.txt", [:append])
-      "ethics" -> {:ok, file} = File.open("ethics.txt", [:append])
+      target when target in ["notes", "programming", "philosophy", "science", "ethics", "god"] -> {:ok, file} = File.open("#{target}.txt", [:append])
       _ -> {:ok, file} = File.open("scratch.txt", [:append])
     end
     write_loop(target, file)
   end
   defp write_loop(target, file) do
-    newline = IO.gets(:stdio, "#{target} * ")
-    IO.write(:stdio, newline)
+    newline = IO.gets(:stdio, "#{target}" <> IO.ANSI.magenta <> " *> " <> IO.ANSI.reset)
+    IO.write(:stdio, IO.ANSI.reverse <> newline <> IO.ANSI.reset)
     IO.write(file, newline)
     write_loop(target, file)
   end
